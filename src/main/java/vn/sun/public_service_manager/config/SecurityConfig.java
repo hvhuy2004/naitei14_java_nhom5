31f7ab1e369b90058fdb6d270eb66e216c07fc9f
NttP0607 <166805936+NttP0607@users.noreply.github.com>
2025-11-27 19:47:23 +0700

    API Lấy thông tin cá nhân, Cập nhật thông tin (trừ số CCCD)

--- src/main/java/vn/sun/public_service_manager/config/SecurityConfig.java
+++ src/main/java/vn/sun/public_service_manager/config/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         // API Auth cho Citizen
-                        .requestMatchers("/api/citizen/auth/**").permitAll()
+                        .requestMatchers("/api/v1/citizen/auth/**").permitAll()
                         // Yêu cầu xác thực cho tất cả các API khác
                         .anyRequest().authenticated()
                 )
